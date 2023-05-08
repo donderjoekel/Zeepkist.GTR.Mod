0bfd57162b0bace6965baefb5c52bfcd1d45f6b2
Christiaan Bloemendaal <github@tnrd.net>
2023-05-08 11:23:48 +0200

    fix: fixed transparency on ghosts

--- Components/Ghosting/GhostPlayer.cs
+++ Components/Ghosting/GhostPlayer.cs
@@ -245,13 +245,13 @@ public class GhostPlayer : MonoBehaviourWithLogging
     private static void ToFadeMode(Material material)
     {
         material.SetOverrideTag("RenderType", "Transparent");
-        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        material.SetInt("_SrcBlend", (int)BlendMode.SrcAlpha);
+        material.SetInt("_DstBlend", (int)BlendMode.OneMinusSrcAlpha);
         material.SetInt("_ZWrite", 0);
         material.DisableKeyword("_ALPHATEST_ON");
         material.EnableKeyword("_ALPHABLEND_ON");
         material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-        material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+        material.renderQueue = (int)RenderQueue.Transparent;
     }
 
     private void DisableRenderers()
