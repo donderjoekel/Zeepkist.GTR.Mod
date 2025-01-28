2d716614b0f602df1a0d9f592b5308e782376c98
Christiaan Bloemendaal <github@tnrd.net>
2025-01-28 09:46:36 +0100

    Voting

---
+++ Commands/Voting/DoubleDownvoteCommand.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TNRD.Zeepkist.GTR.Utilities;
+using TNRD.Zeepkist.GTR.Voting;
+using ZeepSDK.ChatCommands;
+
+namespace TNRD.Zeepkist.GTR.Commands.Voting;
+
+public class DoubleDownvoteCommand : ILocalChatCommand
+{
+    private readonly VotingService _votingService;
+
+    public string Prefix => string.Empty;
+    public string Command => "--";
+    public string Description => "Double downvotes the current map";
+
+    public DoubleDownvoteCommand()
+    {
+        _votingService = ServiceHelper.Instance.GetRequiredService<VotingService>();
+    }
+
+    public void Handle(string arguments)
+    {
+        _votingService.DoubleDownvote();
+    }
+}
---
+++ Commands/Voting/DownvoteCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using TNRD.Zeepkist.GTR.Utilities;
+using TNRD.Zeepkist.GTR.Voting;
+using ZeepSDK.ChatCommands;
+
+namespace TNRD.Zeepkist.GTR.Commands.Voting;
+
+public class DownvoteCommand : ILocalChatCommand
+{
+    private readonly VotingService _votingService;
+
+    public string Prefix => string.Empty;
+    public string Command => "-";
+    public string Description => "Downvotes the current map";
+
+    public DownvoteCommand()
+    {
+        _votingService = ServiceHelper.Instance.GetRequiredService<VotingService>();
+    }
+
+    public void Handle(string arguments)
+    {
+        _votingService.Downvote();
+    }
+}
