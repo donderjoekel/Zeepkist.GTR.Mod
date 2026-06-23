c08a15705986c5ecdf6f22b45debfcd738f9a1be
Christiaan <github@tnrd.net>
2026-06-23 16:26:05 +0200

    fix(timeline): freeze photo mode camera while holding Shift
    Skip flying camera LateUpdate instead of zeroing look input to avoid snapping the view angle.

---
+++ Patching/Patches/FlyingCameraScript_LateUpdate.cs
@@ -0,0 +1,19 @@
+using HarmonyLib;
+using UnityEngine;
+
+namespace TNRD.Zeepkist.GTR.Patching.Patches;
+
+[HarmonyPatch(typeof(FlyingCameraScript), nameof(FlyingCameraScript.LateUpdate))]
+public static class FlyingCameraScript_LateUpdate
+{
+    private static bool Prefix(FlyingCameraScript __instance)
+    {
+        if (__instance.GameMaster == null || !__instance.GameMaster.isPhotoMode)
+            return true;
+
+        if (!Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.RightShift))
+            return true;
+
+        return false;
+    }
+}
